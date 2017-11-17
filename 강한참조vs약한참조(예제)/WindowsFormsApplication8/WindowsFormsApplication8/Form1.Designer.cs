c71db3fc81cb52725077fbed7302ca709395896d
InwooLee <inwoo772@naver.com>
2017-11-17 17:51:54 +0900

    강한참조vs약한참조 예제

---
+++ 강한참조vs약한참조(예제)/WindowsFormsApplication8/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(114, 96);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "simpleButton1";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(47, 24);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(70, 14);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "labelControl1";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(47, 56);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(70, 14);
+            this.labelControl2.TabIndex = 2;
+            this.labelControl2.Text = "labelControl2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+    }
+}
+
---
+++ 강한참조vs약한참조(예제)/WindowsFormsApplication8/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+
+        Animal dog;
+        Animal cat;
+
+        Animal dogRef;
+        WeakReference catRef;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Animal라는 클래스를 만들어 각각 dog, cat의 이름을 만들어 주었습니다.
+            dog = new Animal("Mango");
+            cat = new Animal("KiKi");
+
+            //각각 참조를 거는데 dog은 일반적으로 사용하는 강한참조
+            //cat은 WeakReference를 이용한 약한 참조를 걸어보도록 하겠습니다.
+            dogRef = dog;
+            catRef = new WeakReference(cat);
+
+            //각각 라벨에 출력해보겠습니다.
+            labelControl1.Text = dogRef == null ? "null" : dogRef.name;
+            labelControl2.Text = catRef.Target == null ? "null" : (catRef.Target as Animal).name;
+
+            //당연한 이야기겠지만
+            //------------  label1 = Mango
+            //------------  label2 = KiKi
+            //가 출력됩니다.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Animal
+        {
+            public string name = "";
+            public Animal(string i_name)
+            {
+                this.name = i_name;
+            }
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            //버튼 클릭시 dog과 cat에 null을 넣어 초기화 시켜줍니다.
+            dog = null;
+            cat = null;
+
+            //가바지 컬렉터를 강제로 작동시켜보겠습니다.
+            System.GC.Collect(0, GCCollectionMode.Forced);
+            System.GC.WaitForFullGCComplete();
+
+            //다시한번 참조한 값을 각각 라벨에 출력해보겠습니다.
+            labelControl1.Text = dogRef == null ? "null" : dogRef.name;
+            labelControl2.Text = catRef.Target == null ? "null" : (catRef.Target as Animal).name;
+
+
+            // dogRef과 catRef 모두 참조 객체가 null로 변경이 되었으니 null이 출력된다고 생각했습니다.
+            //하지만 결과는
+            //------------  label1 = Mango
+            //------------  label2 = KiKi
+            //과 같이 출력됩니다.
+            //강한참조는 참조 객체를 초기화 시켜도 Data를 붙잡고 있어서 메모리 회수가 안되는 것입니다.
+        }
+    }
+}
